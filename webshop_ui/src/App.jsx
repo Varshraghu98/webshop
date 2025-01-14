ba43a2200e5f74ff27be53daf7713545f7c919b5
Fahla Parappan <parappanfahla@gmail.com>
2025-01-14 14:12:47 +0100

    About Us

--- webshop_ui/src/App.jsx
+++ webshop_ui/src/App.jsx
@@ -15,6 +15,29 @@ function App() {
         <header>
           <NavBar></NavBar>
         </header>
+        {/* About Us Section */}
+        <main>
+          <section className="about-us">
+            <div className="about-us-content">
+              <h1>Welcome to LowTech GmbH,</h1>
+              <p>
+                where tradition meets innovation. As a proud SME with 45
+                dedicated employees, we specialize in crafting high-quality
+                wooden furnitures. For decades, we built our reputation through direct customer
+                connections, but with changing times, we are now ready to embrace
+                the digital era. Our online store brings our craftsmanship to
+                your fingertips, making it easier than ever to find timeless
+                furniture for your home that suits your comfort.
+              </p>
+              <p>
+                At LowTech GmbH, we blend passion, precision, and sustainability
+                to create furniture that lasts a lifetime. Explore our collection
+                and experience the art of fine woodworking.
+              </p>
+            </div>
+          </section>
+        </main>
+
         <footer>
           <ShopFooter></ShopFooter>
         </footer>
