348ced4680501f8253e7ababd919cb60a1bf8605
Varshini Raghunath <varshraghu@gmail.com>
2025-01-28 10:31:50 +0100

    removing inventary

--- backend/inventory_management/models.py
+++
@@ -1,9 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-#
-# db = SQLAlchemy()
-#
-# class Inventory(db.Model):
-#     __tablename__ = 'inventory'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     product_id = db.Column(db.Integer, nullable=False)
-#     stock_quantity = db.Column(db.Integer, nullable=False)
